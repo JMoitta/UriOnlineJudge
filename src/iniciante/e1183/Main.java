9581aa480a9a607d739c2270536fd00c2e84a768
joao <joao@joao-Lenovo-G50-80>
2018-07-08 15:50:17 -0300

    1184 Abaixo da Diagonal Principal Nova implementacao

--- src/iniciante/e1183/Main.java
+++ src/iniciante/e1183/Main.java
@@ -1,29 +1,134 @@
 package iniciante.e1183;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
+
+	// Declaracao de variaveis
+	private static Scanner scan = new Scanner(System.in);
+	private static String operacao;
+	private static float[][] M;
+	private static float soma;
+
+	// main(args)
 	public static void main(String[] args) {
-		String O;
-		int matriz = 12;
-		double[][] x = new double[matriz][matriz];
-		double soma = 0;
-		int cont = 0;
+		Locale.setDefault(Locale.US);
+		/*
+		 * 1184 Abaixo da Diagonal Principal
+		 */
+		abaixoDaDiagonalPrincipal();
+	}
 
-		Scanner input = new Scanner(System.in);
+	private static void abaixoDaDiagonalPrincipal() {
 
-		O = input.nextLine();
-		for (int i = 0; i < matriz; i++) {
-			for (int j = 0; j < matriz; j++) {
-				x[i][j] = Double.parseDouble(input.nextLine());
-				if (j > i)
-					soma += x[i][j];
+		init();
+
+		process();
+
+		print();
+	}
+
+	private static void init() {
+		operacao = nextString();
+		M = nextMatrizFloat(12);
+		soma = 0;
+	}
+
+	private static void process() {
+		forMatriz(12, (i, j)->{
+			if(i < j) {
+				soma += M[i][j];
+			}
+		});
+		if(operacao.equals("M")) {
+			soma /= 66;
+		}
+	}
+
+	private static void print() {
+		printFloat1(soma);
+	}
+
+	private static void printKeyAndValue(String key, int value) {
+		System.out.printf("%s: %d\n", key, value);
+	}
+	
+	private static void printFloat1(float value) {
+		System.out.printf("%.1f\n", value);
+	}
+
+	private static void printForArray(int[] array, int tamanho, String variavel) {
+		for (int i = 0; i < tamanho; i++) {
+			printArray(i, array[i], variavel);
+		}
+	}
+
+	private static void printArray(int i, int pos, String variavel) {
+		System.out.printf("%s[%d] = %d\n", variavel, i, pos);
+	}
+
+//	private static void nextArrayInt() {
+//		int[] array = new int[10];
+//		for (int i = 0; i < array.length; i++) {
+//			array[i] = converterInteiros(Integer.parseInt(scan.next()));
+//		}
+//		X = array;
+//	}
+
+	private static void forMatriz(int tam, Matriz matriz) {
+		for(int i = 0; i < tam; i++) {
+			for(int j = 0; j < tam; j++) {
+				matriz.matrizBidimensional(i, j);
 			}
 		}
-		if (O.equalsIgnoreCase("S")) {
-			System.out.println(soma);
-		} else if(O.equalsIgnoreCase("M")){
-			System.out.printf("%.1f\n", (soma / 66));
+		
+	}
+	@FunctionalInterface
+	private interface Matriz {
+		public void matrizBidimensional(int i, int j);
+	}
+	private static int converterInteiros(int pos) {
+		if (pos > 0)
+			return pos;
+		return 1;
+	}
+
+	private static float[][] nextMatrizFloat(int tam) {
+		float[][] matriz= new float[tam][tam];
+		for (int i = 0; i < tam; i++) {
+			for(int j = 0; j < tam; j++) {
+				matriz[i][j] = nextFloat();
+			}
 		}
+		return matriz;
+	}
+
+	private static int[] nextArrayInt(int tam) {
+		int[] array = new int[tam];
+		for (int i = 0; i < tam; i++) {
+			array[i] = nextInt();
+		}
+		return array;
+	}
+	
+	private static String nextString() {
+		return scan.next();
+	}
+	
+	private static double nextDouble() {
+		return Double.parseDouble(scan.next());
+	}
+	
+	private static float nextFloat() {
+		return Float.parseFloat(scan.next());
+	}
+
+	private static long nextLong() {
+		return Long.parseLong(scan.next());
+	}
+
+	private static int nextInt() {
+		return Integer.parseInt(scan.next());
 	}
 }
